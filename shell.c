6f375ea7336890e757460faf8d59e046cb7053f3
kanta1025 <infrastructurekanta@gmail.com>
2025-06-23 21:38:55 +0900

    create

--- shell.c
+++ shell.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX_INPUT 1024
+#include <sys/_types/_pid_t.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+#define MAX_INPUT 1024
+#define MAX_ARGS 64
 int main() {
 	char input[MAX_INPUT];
+	char* args[MAX_ARGS];
 
 	while(1) {
 		printf("tsh>");
@@ -14,12 +19,36 @@ int main() {
 			continue;
 		}
 
+		input[strcspn(input, "\n")] = '\0';
+
 		if(strcmp(input, "exit\n") == 0) {
 			printf("bay!\n");
 			break;
 		}
-		
-		printf("%s", input);
+
+		char* token = strtok(input, " ");
+		int i = 0;
+
+		while(token != NULL && i < MAX_ARGS) {
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+
+		args[i] = NULL;
+
+		pid_t pid = fork();
+
+		if(pid == 0) {
+			execvp(args[0], args);
+			perror("execvp failed");
+			exit(EXIT_FAILURE);
+		} else if(pid > 0){
+			int status;
+			waitpid(pid,  &status, 0);
+			printf("Exist status: %d\n, status");
+		} else {
+			perror("fork failed");
+		}
 	}
 	return 0;
 }
