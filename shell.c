20a8fb2fd66acac2c1a1af902741e0a1a3e63a73
kanta1025 <infrastructurekanta@gmail.com>
2025-06-23 21:14:19 +0900

    exit out

--- shell.c
+++ shell.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 #define MAX_INPUT 1024
 
 int main() {
@@ -12,6 +12,11 @@ int main() {
 		if(fgets(input, MAX_INPUT, stdin) == NULL) {
 			perror("fgets failed");
 			continue;
+		}
+
+		if(strcmp(input, "exit\n") == 0) {
+			printf("bay!\n");
+			break;
 		}
 		
 		printf("%s", input);
